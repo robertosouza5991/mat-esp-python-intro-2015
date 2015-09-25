5b50a6f4b47b5dfaee7987ab711db1fe8f1950a6
robertosouza5991 <robertosouza@on.br>
2015-09-25 08:58:30 -0300

    adicionado bubble-sort feito na aula anterior

---
+++ bubble-sort.py
@@ -0,0 +1,12 @@
+# bubble-sort.py
+
+Cartas =  [11, 18, 3, 1, 16, 12, 6, 19, 5, 0, 14, 4, 17, 9, 13, 7, 10, 15, 2, 8]
+N = 20
+For i in range(0,n-1,1):
+    if Cartas[i] > Cartas[i+1]:
+	    temp = Cartas[i]:
+		Cartas[i] = Cartas[j]:
+		Cartas[j] = temp:
+	    test = 1:
+print(Cartas)		
+		
