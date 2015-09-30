d2b2e73880c7a2e916ea0c30d2d7afe2c2a35e62
robertosouza5991 <robertosouza@on.br>
2015-09-30 16:23:33 -0300

    iniciando a parte grafica

--- bubble-sort.py
+++ bubble-sort.py
@@ -1,10 +1,18 @@
 # bubble-sort.py
 
-# declaracao do valor das cartas, e do numero total de caartas
+import pylab as pl
+import numpy as np
+
+# declaracao do valor das cartas, e do numero total de cartas
 Cartas =  [11, 18, 3, 1, 16, 12, 6, 19, 5, 0, 14, 4, 17, 9, 13, 7, 10, 15, 2, 8]
-Maiores(5)
-Menores(5)
+
+
+#declaracao de duas listas vazias para auxilio
+Maiores = [None] * 5
+Menores = [None] * 5
 n = 20
+
+X = np.linspace(0, n-1, n , endpoint=True)
 print ("lista original:",Cartas)
 # para cada carta, primeira carta, ate a penultima
 for i in range(0,n-1,1):
@@ -17,12 +25,19 @@ for i in range(0,n-1,1):
             temp = Cartas[i]
             Cartas[i] = Cartas[j]
             Cartas[j] = temp
+
+			
 print("lista final:",Cartas)
 
-for k in range (0,4,1)
-    Maiores(k) = Cartas(n-5+k)
-    Menores(k) = Cartas(k)
-	
-print("cinco maiores",Maiores)
-print("cinco menores",Menores)		
+#copia os maiores e menores para as listas vazias 
+#for k in range (0,5,1):
+#    Maiores[k] = Cartas[n-5+k]
+#    Menores[k] = Cartas[k]
+#impprime os maiores e menores	
+#print("cinco maiores",Maiores)
+#print("cinco menores",Menores)		
 		
+
+pl.scatter(X, Cartas)
+
+pl.show()		
